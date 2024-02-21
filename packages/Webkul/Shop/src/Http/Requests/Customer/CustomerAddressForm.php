c3d2f058de44c1ea3f9b80625ea3c4965e964f93
Asaduzzaman Abir <anonnaabir00@gmail.com>
2024-02-21 22:29:10 +0600

    Updated

--- packages/Webkul/Shop/src/Http/Requests/Customer/CustomerAddressForm.php
+++ packages/Webkul/Shop/src/Http/Requests/Customer/CustomerAddressForm.php
@@ -90,7 +90,7 @@ class CustomerAddressForm extends FormRequest
             "{$addressType}.city"       => ['required'],
             "{$addressType}.country"    => [new AlphaNumericSpace],
             "{$addressType}.state"      => [new AlphaNumericSpace],
-            "{$addressType}.postcode"   => ['numeric'],
+            "{$addressType}.postcode"   => ['string'],
             "{$addressType}.phone"      => ['required', new PhoneNumber],
         ]);
     }
